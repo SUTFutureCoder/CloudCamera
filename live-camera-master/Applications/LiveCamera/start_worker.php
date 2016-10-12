b01fd74fe5bbf76a8101a6863829b66c6949604c
xingchen <lin1994529@qq.com>
2016-10-12 23:14:37 +0800

    fix firefox bug

--- live-camera-master/Applications/LiveCamera/start_worker.php
+++ live-camera-master/Applications/LiveCamera/start_worker.php
@@ -17,6 +17,8 @@ use \Workerman\Protocols\Websocket;
 
 // 自动加载类
 require_once __DIR__ . '/../../Workerman/Autoloader.php';
+//引入M计划公共类
+//require_once __DIR__ . '';
 
 $recv_worker = new Worker('Websocket://0.0.0.0:8080');
 $recv_worker->onWorkerStart = function($recv_worker)
@@ -34,21 +36,34 @@ $objRedis->set('cloudcamera-test', 0);
 $recv_worker->onMessage = function($connection, $data)use($recv_worker, $objRedis)
 {
     if (!is_object($data)){
-        //尝试获取文件类型
-        $bin     = substr($data, 0, 2);
-        $strinfo = unpack('C2chars', $bin);
-        $typeCode = intval($strinfo['chars1'] . $strinfo['chars2']);
-        switch ($typeCode){
-            case 255216:
-                //jpeg图片类型
-                $picId = $objRedis->incr('cloudcamera-test');
-                file_put_contents('/var/www/html/CloudCamera/temp/test_' . str_pad($picId, 10, '0', STR_PAD_LEFT) . '.jpeg', $data);
-                break;
-            case 8273:
-                //wav音频类型
-                $audioId = $objRedis->incr('cloudcamera-audio-test');
-                file_put_contents('/var/www/html/CloudCamera/temp/audio_test_' . str_pad($audioId, 10, '0', STR_PAD_LEFT) . '.wav', $data);
-                break;
+        if (is_string($data)){
+            //字符串
+            $strDataRec = json_decode($data, true);
+            if ('camera_init' == $strDataRec['type']){
+                //进行验证,返回passport
+                $connection->send(json_encode(array('valid' => 1)));
+                if (empty($strDataRec)){
+                    $connection->send(json_encode(array('valid' => 0)));
+                }
+            }
+        } else {
+            //二进制
+            //尝试获取文件类型
+            $bin     = substr($data, 0, 2);
+            $strinfo = unpack('C2chars', $bin);
+            $typeCode = intval($strinfo['chars1'] . $strinfo['chars2']);
+            switch ($typeCode){
+                case 255216:
+                    //jpeg图片类型
+                    $picId = $objRedis->incr('cloudcamera-test');
+                    file_put_contents('/var/www/html/CloudCamera/temp/test_' . str_pad($picId, 10, '0', STR_PAD_LEFT) . '.jpeg', $data);
+                    break;
+                case 8273:
+                    //wav音频类型
+                    $audioId = $objRedis->incr('cloudcamera-audio-test');
+                    file_put_contents('/var/www/html/CloudCamera/temp/audio_test_' . str_pad($audioId, 10, '0', STR_PAD_LEFT) . '.wav', $data);
+                    break;
+            }
         }
     }
 
@@ -56,8 +71,9 @@ $recv_worker->onMessage = function($connection, $data)use($recv_worker, $objRedi
 //    file_put_contents('/var/www/html/CloudCamera/test.txt', print_r($data, true), FILE_APPEND);
 //    foreach($recv_worker->sendWorker->connections as $send_connection)
 //    {
-        //$send_connection->websocketType = "\x82";
-//        $send_connection->send($data);
+//        var_dump($send_connection);
+////                $send_connection->websocketType = "\x82";
+////        $send_connection->send($data);
 //    }
 };
 
